d5302809a54b2ad3fcdb054efaf9ea216d64f445
Jesse J H Leung <Marduk@Jesses-MacBook-Air.local>
2018-02-05 21:39:53 -1000

    problem solved, basic structure set

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ const inputId = document.getElementById("resourceId");
 const contentId = document.getElementById("contentContainer");
 const typeId = document.getElementById("resourceType");
 let data;
+let builder;
 
 const takeInput = () => {
   let input = inputId.value;
@@ -12,27 +13,41 @@ const takeInput = () => {
 
   if (type === "people") {
     typeURL = "https://swapi.co/api/people";
+    builder = peopleBuilder;
   } else if (type === "planets") {
     typeURL = "https://swapi.co/api/planets";
+    builder = planetBuilder;
   } else if (type === "starships") {
     typeURL = "https://swapi.co/api/starships";
+    builder = starshipBuilder;
   }
-  console.log(typeURL);
   getData(typeURL);
-  console.log("tI data: ", data);
 };
 
 buttonId.addEventListener("click", takeInput);
 
 const getData = tURL => {
-  console.log("tUrl: ", tURL);
-  const reqListener = () => {
+  function reqListener() {
     data = JSON.parse(this.responseText);
     console.log("reqL data: ", data);
-  };
+    builder();
+  }
 
   const oReq = new XMLHttpRequest();
   oReq.addEventListener("load", reqListener);
   oReq.open("GET", tURL);
   oReq.send();
+  return data;
+};
+
+const peopleBuilder = () => {
+  console.log("people");
+};
+
+const planetBuilder = () => {
+  console.log("planet");
+};
+
+const starshipBuilder = () => {
+  console.log("starship");
 };
