d87df8b13c4fa9448ce7eae9f2bb9e1f6e69a4f3
Jesse J H Leung <Marduk@Jesses-MacBook-Air.local>
2018-02-07 00:23:47 -1000

    working but problems with async

--- js/app.js
+++ js/app.js
@@ -2,14 +2,18 @@ const buttonId = document.getElementById("requestResourceButton");
 const inputId = document.getElementById("resourceId");
 const contentId = document.getElementById("contentContainer");
 const typeId = document.getElementById("resourceType");
+let input;
+let type;
 let data;
+let deepData;
+let filmList = [];
+let needFilms = false;
 let builder;
 
 const takeInput = () => {
-  let input = inputId.value;
-  let type = typeId.value;
+  input = inputId.value;
+  type = typeId.value;
   let typeURL;
-  contentId.innerHTML = input;
 
   if (type === "people") {
     typeURL = "https://swapi.co/api/people";
@@ -28,8 +32,10 @@ buttonId.addEventListener("click", takeInput);
 
 const getData = tURL => {
   function reqListener() {
-    data = JSON.parse(this.responseText);
+    data = JSON.parse(this.responseText).results;
     console.log("reqL data: ", data);
+    console.log("input: ", input);
+    let myData = null;
     builder();
   }
 
@@ -37,17 +43,108 @@ const getData = tURL => {
   oReq.addEventListener("load", reqListener);
   oReq.open("GET", tURL);
   oReq.send();
-  return data;
 };
 
 const peopleBuilder = () => {
   console.log("people");
+  contentId.innerHTML = "";
+  if (checkData()) {
+    eleMaker(contentId, "h2", null, null, myData.name);
+    eleMaker(contentId, "p", null, null, myData.gender);
+    eleMaker(contentId, "p", null, null, getDeeperData(myData.species[0]).name);
+    myData = null;
+  } else {
+    throw new Error('"' + input + '" not found');
+  }
 };
 
 const planetBuilder = () => {
   console.log("planet");
+  contentId.innerHTML = "";
+  needFilms = true;
+  if (checkData()) {
+    eleMaker(contentId, "h2", null, null, myData.name);
+    eleMaker(contentId, "p", null, null, myData.terrain);
+    eleMaker(contentId, "p", null, null, myData.population);
+    addFilms();
+    myData = null;
+  } else {
+    throw new Error('"' + input + '" not found');
+  }
 };
 
 const starshipBuilder = () => {
   console.log("starship");
+  contentId.innerHTML = "";
+  needFilms = true;
+  if (checkData()) {
+    eleMaker(contentId, "h2", null, null, myData.name);
+    eleMaker(contentId, "p", null, null, myData.manufacturer);
+    eleMaker(contentId, "p", null, null, myData.starship_class);
+    addFilms();
+    myData = null;
+  } else {
+    throw new Error('"' + input + '" not found');
+  }
+};
+
+const checkData = () => {
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].name === input) {
+      myData = data[i];
+      console.log("myData: ", myData);
+      getFilms();
+      return true;
+    }
+  }
+};
+
+const getDeeperData = tURL => {
+  function deepListener() {
+    deepData = JSON.parse(this.responseText);
+    console.log(needFilms);
+    if (needFilms === true) {
+      filmList.push(deepData.title);
+    }
+    console.log(deepData);
+    console.log("film title: ", deepData.title);
+  }
+  const deepReq = new XMLHttpRequest();
+  deepReq.addEventListener("load", deepListener);
+  deepReq.open("GET", tURL);
+  deepReq.send();
+  return deepData;
+};
+
+const getFilms = () => {
+  for (let g = 0; g < myData.films.length; g++) {
+    getDeeperData(myData.films[g]);
+  }
+  console.log("films got: ", filmList);
+};
+
+const addFilms = () => {
+  console.log("adding");
+  let uList = eleMaker(contentId, "ul", "listId");
+  for (let f = 0; f < filmList.length; f++) {
+    eleMaker(uList, "li", null, null, filmList[f]);
+    console.log("fl: ", filmList[f]);
+  }
+  filmList = [];
+};
+
+const eleMaker = (parent, ele, id, cls, inner) => {
+  let myEle = document.createElement(ele);
+  if (id) {
+    myEle.id = id;
+  }
+  if (cls) {
+    myEle.class = cls;
+  }
+  if (inner) {
+    myEle.innerHTML = inner;
+  }
+
+  parent.appendChild(myEle);
+  return document.getElementById(id);
 };
